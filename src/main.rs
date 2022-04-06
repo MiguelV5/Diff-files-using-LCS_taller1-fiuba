796b462eac723c1630115722929fd65a3299f330
MiguelV5 <mvasquezj@fi.uba.ar>
2022-04-06 15:11:47 -0300

    modularized main with ejecutar_diff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+
 use std::fs::File;
 use std::io::Error;
 use std::io::Read;
@@ -43,25 +44,38 @@ fn main() {
         Some((path_archivo_1, path_archivo_2)) => (path_archivo_1, path_archivo_2),
         None => process::exit(CODIGO_DE_SALIDA_POR_ERROR),
     };
+
+    match ejecutar_diff(path_archivo_1, path_archivo_2) {
+        Ok(()) => (),
+        Err(string_con_info_de_error) => {
+            println!("{}", string_con_info_de_error);
+            process::exit(CODIGO_DE_SALIDA_POR_ERROR);
+        }
+    }
+
+}
+
+
+fn ejecutar_diff(path_archivo_1: String, path_archivo_2: String) -> Result<(), String> {
+
     
     let lineas_f1: Vec<String> = match read_file_lines(&path_archivo_1) {
         Ok(lineas) => lineas,
         Err(err) => {
-            println!("Error al obtener lineas del primer archivo pasado por parametro.\nDetalle de error:\n {:?}", err);
-            process::exit(CODIGO_DE_SALIDA_POR_ERROR);
+            return Err(format!("Error al obtener lineas del primer archivo pasado por parametro.\nDetalle de error:\n {:?}", err));
         }
     };
 
     let lineas_f2: Vec<String> = match read_file_lines(&path_archivo_2) {
         Ok(lineas) => lineas,
         Err(err) => {
-            println!("Error al obtener lineas del segundo archivo pasado por parametro.\nDetalle de error:\n {:?}", err);
-            process::exit(CODIGO_DE_SALIDA_POR_ERROR);
+            return Err(format!("Error al obtener lineas del segundo archivo pasado por parametro.\nDetalle de error:\n {:?}", err));
         }
     };
 
     println!("Lecturas del primer archivo: {:?}", lineas_f1);
     println!("Lecturas del segundo archivo: {:?}", lineas_f2);
 
+    Ok(())
 
 }
