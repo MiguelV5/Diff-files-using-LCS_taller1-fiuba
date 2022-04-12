43395c54e0704a941ea8ce913430355cb6aca1e4
MiguelV5 <mvasquezj@fi.uba.ar>
2022-04-11 22:48:54 -0300

    cambiado process:exit ---> return de un Result en el main

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ use std::env;
 mod lib;
 use lib::*;
 
-fn main() {
+const EXIT_ERROR_CODE: i32 = 1;
+
+fn main() -> Result<(), i32> {
     let prompt_args: Vec<String> = env::args().collect();
 
     let (path_of_file_1, path_of_file_2) = match parse_args(&prompt_args) {
         Some((path_of_file_1, path_of_file_2)) => (path_of_file_1, path_of_file_2),
         None => {
             println!("\nSe recibieron demasiados/muy pocos argumentos.\nSe necesita el nombre de los dos archivos a procesar.\nArgumentos recibidos: {:?}\n", prompt_args);
-            return;
+            return Err(EXIT_ERROR_CODE);
         }
     };
 
@@ -19,9 +21,11 @@ fn main() {
             Ok((lines_f1, lines_f2)) => (lines_f1, lines_f2),
             Err(string_with_error_info) => {
                 println!("\n{}\n", string_with_error_info);
-                return;
+                return Err(EXIT_ERROR_CODE);
             }
         };
 
     run_diff(lines_of_file_1, lines_of_file_2);
+
+    Ok(())
 }
